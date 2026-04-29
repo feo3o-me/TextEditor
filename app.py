de5618a3c7db4b02107416ca28a6b2bb6313e646
feo3o <feo3o.me@gmail.com>
2026-04-29 13:24:32 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import tkinter as tk # GUI library
+
+def main():
+    # =========== #
+    # Window settings
+    
+    window = tk.Tk()
+    window.title("Notepad--")
+
+    # =========== #
+    # Main loop
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
