9ff21a77ddce08dff6f5c5db1b14611212d977b5
feo3o <feo3o.me@gmail.com>
2026-04-29 18:09:28 -0300

    Now you can change the font

--- app.py
+++ app.py
@@ -2,27 +2,6 @@ import tkinter as tk # GUI library
 from tkinter.filedialog import askopenfilename, asksaveasfilename # File management
 from tkinter import ttk
 
-def font_selector(text_editor):
-    new_window = tk.Tk()
-    new_window.geometry("320x240")
-
-    ttk.Label(new_window, text = "Selecione a fonte: ").grid(row = 2, column = 1, padx=20, pady=10)
-
-    select = tk.StringVar()
-    font_box = ttk.Combobox(new_window, textvariable=select)
-    font_box['values'] = (
-    'Arial',
-    'Courier New',
-    'Calambria',
-    'Tahoma',
-    'Times New Roman'
-            )
-    font_box.grid(row = 2, column = 2)
-    font_box.current()
-    
-    send = tk.Button(new_window, text = "Ok", relief=tk.GROOVE, command = lambda: text_editor.configure(font="Courier New, 18"))
-    send.grid(row = 6, column=2)
-
 def open_file(window, text_editor):
     file_path = askopenfilename(filetypes=[("Arquivos de Texto", "*.txt")])
 
@@ -51,11 +30,12 @@ def main():
     window = tk.Tk()
     window.title("Notepad--")
     window.resizable(False, False)
+    window.geometry("640x480")
 
     # =========== #
     # Widgets
 
-    text_editor = tk.Text(window, font="Arial 12", padx = 3)
+    text_editor = tk.Text(window, font=("Arial", 14), padx = 3)
     text_editor.grid(row = 1, column = 0)
     
     # =========== #
@@ -70,6 +50,30 @@ def main():
     save_btn.grid(row = 0, column = 1)
     font_btn.grid(row = 0, column = 2)
     btn_frame.grid(row = 0, column= 0, sticky="W")
+
+    def font_selector(text_editor):
+        new_window = tk.Tk()
+        new_window.geometry("320x240")
+
+        ttk.Label(new_window, text = "Selecione a fonte: ").grid(row = 2, column = 1, padx=20, pady=10)
+
+        def change_font():
+            font = font_box.get()
+            text_editor.configure(font=(f"{font}", 14))
+
+        font_box = ttk.Combobox(new_window)
+        font_box['values'] = (
+        'Arial',
+        'Courier New',
+        'Tahoma',
+        'Times New Roman',
+        'Comic Sans MS'
+                )
+        font_box.grid(row = 2, column = 2)
+        font_box.current(0)
+        
+        send = tk.Button(new_window, text = "Ok", relief=tk.GROOVE, command= lambda: change_font())
+        send.grid(row = 6, column=2)
 
     # =========== #
     # Main loop
